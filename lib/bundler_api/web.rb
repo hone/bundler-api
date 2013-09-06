2e37b601c9d78744bae19265f9f4bfe571216e34
Terence Lee <hone02@gmail.com>
2013-09-06 14:46:10 -0500

    implement web component of names.list

--- lib/bundler_api/web.rb
+++ lib/bundler_api/web.rb
@@ -10,6 +10,7 @@ require 'bundler_api/honeybadger'
 require 'bundler_api/gem_helper'
 require 'bundler_api/update/job'
 require 'bundler_api/update/yank_job'
+require 'bundler_api/v2_db'
 
 
 class BundlerApi::Web < Sinatra::Base
@@ -131,6 +132,12 @@ class BundlerApi::Web < Sinatra::Base
 
       json_payload(payload)
     end
+  end
+
+  get "/api/v2/names.list" do
+    content_type 'text/plain'
+
+    BundlerApi::V2DB.new(@conn).names.join("\n")
   end
 
   get "/quick/Marshal.4.8/:id" do
